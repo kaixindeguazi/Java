f4f4e6ec99367aec6def5ce171889323407220e9
kaixindeguazi <1009641444@qq.com>
2017-06-24 16:30:59 +0800

    基础语法

--- Hello.java
+++ Hello.java
@@ -10,5 +10,30 @@ public class Hello{
 }
 
 class Person{
+	public static void main(String[] args){
+		//定义整型： byte（-128~+127） short int long
+		byte b1 = 12;
+		//byte b1 = 128     ×
+		short b2 = 128;
+		int b3 = 128;
+		//定义long型变量时，值的末尾加"L"或"l";
+		long b4 = 21321232L;
+		System.out.println(b1+"\n"+b2+"\n"+b3+"\n"+b4);
 
+		//浮点型（带小数点的数值）float  double
+		double d1 = 12.3;
+		//声明float类型的浮点型数据，末尾要加"F"或"f";
+		float d2 = 12.3F;
+		System.out.println(d1+"\n"+d2);
+
+		//字符型: char
+		char c1 = 'a';
+		//char c2 = 'ab';     ×
+		String c2 = "abc";
+		char c3 =  '中';
+		String str = "中国";
+		//可以表示转义字符
+		char c4 = '\t';	
+		System.out.println(c1+"\n"+c2+"\n"+c3+"\n"+str);
+	}
 }
